dd84ac6b6e88788e3709e142cb02ebf1e0198221
BartoszKuziemski <bartuspl99@gmail.com>
2022-12-09 16:06:18 +0100

    chart with mocked data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ChartModule} from "primeng/chart";
+import {ToastModule} from "primeng/toast";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ChartModule,
+    ToastModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
